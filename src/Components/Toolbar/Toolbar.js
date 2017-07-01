a609d0c72ffc65ca1a5e7104d905693ff578e58f
Brett Celestre <bcelestre@gmail.com>
2017-06-30 22:02:40 -0700

    Inits Toolbar

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import './Toolbar.css';
-import '../App/App.css';
 
 class Toolbar extends Component {
 
+  // constructor(props){
+    // super(props)
+    // this.props.current = 1;
+    // this.props.galleryLength = 6;
+  // }
+
   render() {
     return (
-      <div className="container toolbar">
-        <div className="title-year row">
-          <div className="u-full-width">
-            <span className="title">The Casino</span> <span className="year">- 2017</span>
+      <div className="outer">
+        <div className="container toolbar">
+          <div className="title-year row">
+            <div className="u-full-width">
+              <span className="title">The Casino</span> <span className="year">- 2017</span>
+              {/* <span className="galleryIndex">{props.current}/{props.galleryLength}</span> */}
+              <span className="galleryIndex">1/8</span>
+            </div>
           </div>
-        </div>
-        <div className="info row">
-          <div className="u-full-width">
-            <span className="description">Oil on Canvas</span>
+          <div className="info row">
+            <div className="u-full-width">
+              <span className="description">Oil on Canvas</span>
+            </div>
           </div>
         </div>
       </div>
