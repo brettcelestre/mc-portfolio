dc7b24545dc2aff39c37aae1a082bc671e45467e
Brett Celestre <bcelestre@gmail.com>
2017-06-30 22:01:45 -0700

    adds props

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './Content.css';
-import bio from './matthew_celestre_painting_01.jpg';
 
-import Toolbar from '../Toolbar/Toolbar';
+import Gallery from '../Gallery/Gallery.js';
 
 class Content extends Component {
 
   render() {
     return (
       <div className="content">
-        <img src={bio} alt="hi" className="gallery-image"/>
 
-        <Toolbar />
+        <Gallery />
 
       </div>
     );
@@ -22,4 +20,4 @@ class Content extends Component {
 export default Content;
 
 
- // width="1500" height="1077" 
+ // width="1500" height="1077"
