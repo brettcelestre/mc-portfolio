6723fa395e59dfe3a69f31384bb66d57e13ee965
Brett Celestre <bcelestre@gmail.com>
2017-07-18 20:05:40 -0700

    Updates routes and main styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App/App.js';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 import './assets/css/skeleton.css';
 import './assets/css/normalize.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './Components/App/App.js';
+import About from './Components/About/About.js';
+import Toolbar from './Components/Toolbar/Toolbar.js';
+import Home from './Components/Home/Home.js';
+import Links from './Components/Links/Links.js';
+import Store from './Components/Store/Store.js';
+import Music from './Components/Music/Music.js';
+import Gallery from './Components/Gallery/Gallery.js';
+import ShortFilms from './Components/ShortFilms/ShortFilms.js';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div className="nav-bar-spacer">
+      <Route path="/" component={App}>
+      </Route>
+      <Switch>
+        <Route path="/artwork/watercolors" component={Gallery} />
+        <Route path="/artwork/markers" component={Gallery} />
+        <Route path="/artwork/cross-hatchings" component={Gallery} />
+        <Route path="/artwork/stipplings" component={Gallery} />
+        <Route path="/artwork/stipplings/:stippling" component={Gallery} />
+        <Route path="/artwork/paintings" component={Gallery} />
+        <Route path="/artwork/paintings/:painting" component={Gallery} />
+        <Route path="/short-films" component={ShortFilms} />
+        <Route path="/short-films/:film" component={ShortFilms} />
+        <Route path="/music" component={Music} />
+        <Route path="/store" component={Store} />
+        <Route path="/links" component={Links} />
+        <Route path="/about" component={About} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
