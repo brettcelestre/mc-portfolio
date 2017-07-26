82b934e680be0c019e0c4fa604819581e7524c2d
Brett Celestre <bcelestre@gmail.com>
2017-07-26 08:38:51 -0700

    ImageViewer updates

--- src/Components/ImageViewer/ImageViewer.js
+++ src/Components/ImageViewer/ImageViewer.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 // import Image from 'react-image-resizer';
+// import Img from 'react-image'
 import './ImageViewer.css';
 
 import example from '../../assets/gallery/example/example.json';
 import bio from '../../assets/img/bio/matthew_celestre_painting_01.jpg';
-
-import grant_small from '../../assets/gallery/example/Grant_0_Small.jpg';
-import grant_medium from '../../assets/gallery/example/Grant_0_Medium.jpg';
-import grant_large from '../../assets/gallery/example/Grant_0_Large.jpg';
+//
+// import grant_small from '../../assets/gallery/example/Grant_0_Small.jpg';
+// import grant_medium from '../../assets/gallery/example/Grant_0_Medium.jpg';
+// import grant_large from '../../assets/gallery/example/Grant_0_Large.jpg';
 
 class ImageViewer extends Component {
 
   componentDidMount(){
 
-    // console.log('example data', example.data[0]);
+    console.log('example data', example.data[0]);
     // let test = require(`${example.data[0]}_Large.jpg`);
     // console.log('SRC', example.data[0].src + this.state.currentSize);
+    console.log('width', example.data[0].sizes[this.state.currentSize].width);
+    console.log('height', example.data[0].sizes[this.state.currentSize].height);
+    this.setState({
+      width: example.data[0].sizes[this.state.currentSize].width,
+      height: example.data[0].sizes[this.state.currentSize].height});
+    // this.setState({width: example.data[0].sizes[this.state.currentSize].width});
     // console.log('this.state.SRC', this.state.SRC);
     // this.setState({data: example.data[0]});
 
     // let windowSize = '_Large.jpg';
     // this.setState({SRC: example.data[0].src + windowSize});
+
+    console.log('this.state.width', this.state.width);
   }
 
 
@@ -31,29 +40,33 @@ class ImageViewer extends Component {
       'src': '',
       'date': '',
       'description': '',
-      'currentSize': 'Large.jpg',
+      'currentSize': 'large',
       'width': 0,
       'height': 0,
       SRC: 'temp'
     }
 
     this.previous = this.previous.bind(this);
+    this.next = this.next.bind(this);
   }
 
   previous() {
+    console.log('---------');
     console.log('previous');
-    console.log('this.state.SRC', this.state.SRC);
+    console.log('this.state', this.state);
   }
 
   next() {
+    console.log('---------');
     console.log('next');
+    console.log('this.state.SRC', this.state.SRC);
   }
 
   render() {
 
-
     return (
       <div className="image-viewer">
+
         <div
             className="previous"
             onClick={this.previous}>
@@ -64,28 +77,37 @@ class ImageViewer extends Component {
             alt="previous"
             className="previousSVG"/>
         </div>
-
         <div
             className="next"
             onClick={this.next}>
         </div>
 
-        {/* <Image */}
-          {/* // src={require("../../assets/img/bio/matthew_celestre_painting_01.jpg")} */}
-          {/* // src="../../assets/img/bio/matthew_celestre_painting_01.jpg" */}
-          {/* src={bio} */}
-
-          {/* width={1500} */}
-          {/* height={1077} */}
-          {/* // src={require(`${this.state.SRC}`)} */}
-          {/* // src={`${this.state.SRC}`} */}
-          {/* // src={this.src} */}
-          {/* // style={style.image} */}
-        {/* /> */}
-        {/*<img
+        {/* <Image
+          // src={require("../../assets/img/bio/matthew_celestre_painting_01.jpg")}
+          // src="../../assets/img/bio/matthew_celestre_painting_01.jpg"
           src={bio}
+
+          // width={1500}
+          // height={1077}
+
+          width={this.state.width}
+          height={this.state.height}
+          // src={require(`${this.state.SRC}`)}
+          // src={`${this.state.SRC}`}
+          // src={this.src}
+          // style={style}
           className="gallery-image"
-          alt="picture" width="1500" height="1077" />*/}
+        />*/}
+        <img
+          // src={bio}
+          src={require("./matthew_celestre_painting_01.jpg")}
+          width={444}
+          height={444}
+          // width={this.state.width}
+          // height={this.state.height}
+          className="gallery-image"
+          alt="picture"
+        />
       </div>
     );
   }
@@ -93,4 +115,3 @@ class ImageViewer extends Component {
 }
 
 export default ImageViewer;
-/////////
