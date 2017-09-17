e98d1a9aead918e44d31f9eee469ccdc6a95d49c
Brett Celestre <bcelestre@gmail.com>
2017-09-17 13:26:05 -0700

    Adds links

--- src/Components/Links/Links.js
+++ src/Components/Links/Links.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import './Links.css';
+
+import links from '../../assets/data/links.json';
 
 class Links extends Component {
 
   render() {
+    const linkItems = links.map((link, i) => {
+      return (
+        <a href={link.url} key={i}>
+          <div className='linkItem'>
+            <h2 className='title'>{link.title}</h2>
+            <h4 className='description'>{link.description}</h4>
+          </div>
+        </a>
+      )
+    });
+
     return (
-      <div>
-        <h1>Links</h1>
-        <h1>Links</h1>
-        <h1>Links</h1>
-        <h1>Links</h1>
-        <h1>Links</h1>
-        <h1>Links</h1>
-        <h1>Links</h1>
-        <h1>Links</h1>
+      <div className="container linkSection">
+        {linkItems}
       </div>
     );
   }
--- src/assets/data/links.js
+++
@@ -1,8 +0,0 @@
-modules.export = [
-  {
-    "url": "www.google.com",
-    "title": "Google",
-    "description": "wagag asogagiahweogh g a g ad"
-  },
-  {}
-]
