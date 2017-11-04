8cef6feb278cd08935bf94ab189f8ddeab3dbde7
Brett Celestre <bcelestre@gmail.com>
2017-11-04 15:45:24 -0700

    Adds more image data

---
+++ src/assets/data/paintings.js
@@ -0,0 +1,254 @@
+module.exports = {
+  "data": [
+    // {
+    //   "title": "The Casino",
+    //   "date": "2017",
+    //   "description": "OIL ON CANVAS | 6' x 4'",
+    //   "arrows": "light",
+    //   "info": "",
+    //   "sizes": {
+    //       "zoom": {
+    //         "width": 100,
+    //         "height": 100
+    //       },
+    //       "large": {
+    //         "width": 1446,
+    //         "height": 1200
+    //       },
+    //       "medium": {
+    //         "width": 1085,
+    //         "height": 900
+    //       },
+    //       "small": {
+    //         "width": 723,
+    //         "height": 600
+    //       }
+    //     }
+    // },
+    // {
+    //   "title": "Madrid",
+    //   "date": "2017",
+    //   "description": "OIL ON CANVAS | 6' x 4'",
+    //   "arrows": "light",
+    //   "info": "",
+    //   "sizes": {
+    //       "zoom": {
+    //         "width": 100,
+    //         "height": 100
+    //       },
+    //       "large": {
+    //         "width": 1446,
+    //         "height": 1200
+    //       },
+    //       "medium": {
+    //         "width": 1085,
+    //         "height": 900
+    //       },
+    //       "small": {
+    //         "width": 723,
+    //         "height": 600
+    //       }
+    //     }
+    // },
+    // {
+    //   "title": "The Roaring Twenties",
+    //   "date": "2017",
+    //   "description": "OIL ON CANVAS | 6' x 4'",
+    //   "arrows": "light",
+    //   "info": "",
+    //   "sizes": {
+    //       "zoom": {
+    //         "width": 100,
+    //         "height": 100
+    //       },
+    //       "large": {
+    //         "width": 1446,
+    //         "height": 1200
+    //       },
+    //       "medium": {
+    //         "width": 1085,
+    //         "height": 900
+    //       },
+    //       "small": {
+    //         "width": 723,
+    //         "height": 600
+    //       }
+    //     }
+    // },
+    {
+      "title": "Somnolence",
+      "date": "2013",
+      "description": "OIL ON CANVAS",
+      "arrows": "light",
+      "info": "",
+      "sizes": {
+        "zoom": {
+          "width": 100,
+          "height": 100
+        },
+        "large": {
+          "width": 1498,
+          "height": 1200
+        },
+        "medium": {
+          "width": 1100,
+          "height": 881
+        },
+        "small": {
+          "width": 749,
+          "height": 600
+        }
+      }
+    },
+    // {
+    //   "title": "Goombah Lord",
+    //   "date": "2010",
+    //   "description": "OIL ON CANVAS | 6' x 4'",
+    //   "arrows": "light",
+    //   "info": "",
+    //   "sizes": {
+    //       "zoom": {
+    //         "width": 100,
+    //         "height": 100
+    //       },
+    //       "large": {
+    //         "width": 1446,
+    //         "height": 1200
+    //       },
+    //       "medium": {
+    //         "width": 1085,
+    //         "height": 900
+    //       },
+    //       "small": {
+    //         "width": 723,
+    //         "height": 600
+    //       }
+    //     }
+    // },
+    {
+      "title": "Oaklahoma City",
+      "date": "2009",
+      "description": "OIL ON CANVAS",
+      "arrows": "light",
+      "info": "",
+      "sizes": {
+          "zoom": {
+            "width": 100,
+            "height": 100
+          },
+          "large": {
+            "width": 1023,
+            "height": 1200
+          },
+          "medium": {
+            "width": 767,
+            "height": 900
+          },
+          "small": {
+            "width": 512,
+            "height": 600
+          }
+        }
+    },
+    {
+      "title": "Mother",
+      "date": "2009",
+      "description": "OIL ON CANVAS",
+      "arrows": "light",
+      "info": "",
+      "sizes": {
+          "zoom": {
+            "width": 100,
+            "height": 100
+          },
+          "large": {
+            "width": 945,
+            "height": 1200
+          },
+          "medium": {
+            "width": 709,
+            "height": 900
+          },
+          "small": {
+            "width": 472,
+            "height": 600
+          }
+        }
+    },
+    {
+      "title": "I Was Cured",
+      "date": "2009",
+      "description": "OIL ON CANVAS",
+      "arrows": "light",
+      "info": "",
+      "sizes": {
+          "zoom": {
+            "width": 100,
+            "height": 100
+          },
+          "large": {
+            "width": 1500,
+            "height": 1112
+          },
+          "medium": {
+            "width": 1100,
+            "height": 815
+          },
+          "small": {
+            "width": 800,
+            "height": 593
+          }
+        }
+    },
+    {
+      "title": "Italy",
+      "date": "2009",
+      "description": "OIL ON CANVAS",
+      "arrows": "light",
+      "info": "",
+      "sizes": {
+          "zoom": {
+            "width": 100,
+            "height": 100
+          },
+          "large": {
+            "width": 949,
+            "height": 1200
+          },
+          "medium": {
+            "width": 712,
+            "height": 900
+          },
+          "small": {
+            "width": 474,
+            "height": 600
+          }
+        }
+    },
+    {
+      "title": "Winter Fruit",
+      "date": "2007",
+      "description": "OIL ON CANVAS",
+      "arrows": "light",
+      "info": "",
+      "sizes": {
+          "zoom": {
+            "width": 100,
+            "height": 100
+          },
+          "large": {
+            "width": 1446,
+            "height": 1200
+          },
+          "medium": {
+            "width": 1085,
+            "height": 900
+          },
+          "small": {
+            "width": 723,
+            "height": 600
+          }
+        }
+    }
+  ]
+}
