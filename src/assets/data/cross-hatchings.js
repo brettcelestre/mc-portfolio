18576810495ea2580b651695b1afff539bb30533
Brett Celestre <bcelestre@gmail.com>
2017-11-04 17:35:08 -0700

    Adds cross hatching data

---
+++ src/assets/data/cross-hatchings.js
@@ -0,0 +1,105 @@
+module.exports = {
+    "data": [
+      {
+        "title": "Dewitt Study 4",
+        "date": "2014",
+        "description": "ULTRA FINE SHARPIES",
+        "arrows": "dark",
+        "info": "",
+        "sizes": {
+            "zoom": {
+              "width": 999,
+              "height": 100
+            },
+            "large": {
+              "width": 890,
+              "height": 1200
+            },
+            "medium": {
+              "width": 667,
+              "height": 900
+            },
+            "small": {
+              "width": 445,
+              "height": 600
+            }
+          }
+      },
+      {
+        "title": "Dewitt Study 3",
+        "date": "2014",
+        "description": "ULTRA FINE SHARPIES",
+        "arrows": "dark",
+        "info": "",
+        "sizes": {
+            "zoom": {
+              "width": 999,
+              "height": 100
+            },
+            "large": {
+              "width": 904,
+              "height": 1200
+            },
+            "medium": {
+              "width": 678,
+              "height": 900
+            },
+            "small": {
+              "width": 452,
+              "height": 600
+            }
+          }
+      },
+      {
+        "title": "Dewitt Study 2",
+        "date": "2014",
+        "description": "ULTRA FINE SHARPIES",
+        "arrows": "dark",
+        "info": "",
+        "sizes": {
+            "zoom": {
+              "width": 999,
+              "height": 100
+            },
+            "large": {
+              "width": 889,
+              "height": 1200
+            },
+            "medium": {
+              "width": 667,
+              "height": 900
+            },
+            "small": {
+              "width": 445,
+              "height": 600
+            }
+          }
+      },
+      {
+        "title": "Dewitt Study 1",
+        "date": "2013",
+        "description": "ULTRA FINE SHARPIES",
+        "arrows": "dark",
+        "info": "",
+        "sizes": {
+            "zoom": {
+              "width": 999,
+              "height": 100
+            },
+            "large": {
+              "width": 924,
+              "height": 1200
+            },
+            "medium": {
+              "width": 693,
+              "height": 900
+            },
+            "small": {
+              "width": 462,
+              "height": 600
+            }
+          }
+      }
+    ]
+  }
+  
