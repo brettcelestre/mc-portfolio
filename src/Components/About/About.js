092dc760cb819542147c70d96f456a4eb66d17ec
Brett Celestre <bcelestre@gmail.com>
2017-11-04 17:36:04 -0700

    Updates about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,7 +18,7 @@ class About extends Component {
           />
         </div>
         <div className="row bio">
-          Matthew Celestre is a self taught artist from Pleasant Hill, California. Born March 30, 1987, he began drawing in early elementary school after looking and copying drawings from countless art books. Since the age of 12, he has suffered from Bipolar Disorder. Matthew copes with his illness by using multiple forms of art, as a therapeutic escape, to express vivid and painstakingly detailed moments of love, beauty, pain and lust.
+          Matthew Celestre is a self taught artist from Pleasant Hill, California.
         </div>
       </div>
     );
