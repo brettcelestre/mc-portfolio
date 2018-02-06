5d9fa636a36f005afca70cd5c99ccbc436845f13
Brett Celestre <bcelestre@gmail.com>
2018-02-06 10:44:41 -0800

    adds zoom and media styles to toolbar

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -25,12 +25,11 @@ class Toolbar extends Component {
     return (
       <div className="toolbar">
         <div className="container">
-
-          <div className="u-full-width">
+          <div className="u-full-width toolbar-desktop">
 
             <div className="eight columns title-year">
-              <span className="title">{this.props.imageData.name}</span> <span className="year">- {this.props.imageData.date}</span><br />
-              <span className="description">{this.props.imageData.description}</span>
+              <span className="title">{this.props.imageData.name}</span> <span className="year year-top">- {this.props.imageData.date}</span><br />
+              <span className="description">{this.props.imageData.description} <span className="year year-bottom">- {this.props.imageData.date}</span></span>
             </div>
 
             <div className="four columns tools">
@@ -50,3 +49,21 @@ class Toolbar extends Component {
 }
 
 export default Toolbar;
+
+
+
+{/* <div className="u-full-width toolbar-mobile">
+<div className="title-year">
+    <span className="title">{this.props.imageData.name}</span> <span className="year">- {this.props.imageData.date}</span><br />
+    <span className="description">{this.props.imageData.description}</span>
+  </div>
+
+  <div className="tools">
+    <div className="zoom-button" onClick={this.zoomToggle}>
+      <img className="zoom-icon" src={zoomIcon} width="30px" height="30px" />
+    </div>
+    <div className="gallery-index">
+      {galleryPosition}/{this.props.imageData.galleryLength}
+    </div>
+  </div>
+</div> */}
